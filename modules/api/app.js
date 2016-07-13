e4c9c243ce51bad1f0e92fd5b2c96ba1220d3faa
Zippi-wzp <truebatmandc@gmail.com>
2016-07-14 01:02:16 +0300

    Connected  to data base. Injecting the model configuration into routers

--- modules/api/app.js
+++ modules/api/app.js
@@ -10,18 +10,27 @@ var debug = require('debug')('app4');
 
 var app = express();
 
+var model = require('./models-postgre/notes');
+model.connect("postgres://bruce_wayne:43873a65@localhost:5432/gothamdb", function (err) {
+  if (err) throw err;
+});
+
+[index, notes].forEach(function (router) {
+  router.configure({model: model});
+});
+
 // view engine setup
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, './public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', index);
 app.use('/', notes);
@@ -40,7 +49,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('showerror', {
       message: err.message,
       error: err
     });
