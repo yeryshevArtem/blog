01c1ebf29cf2cbd6ca245f9c7e9b640a7e795eac
Zippi-wzp <truebatmandc@gmail.com>
2016-07-14 18:13:09 +0300

    Rename entity 'note' to 'post'

--- modules/api/routes/index.js
+++ modules/api/routes/index.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var notes = undefined;
+var posts = undefined;
 module.exports = router;
 module.exports.configure = function (params) {
-  notes = params.model;
+  posts = params.model;
 }
 
 router.get('/', function (req, res) {
-  notes.titles(function (err, titles) {
+  posts.titles(function (err, titles) {
     if (err) {
       res.render('showerror', {
-        title: 'Could not retrieve note keys from data store',
+        title: 'Could not retrieve post keys from data store',
         error: err
       });
     } else {
       res.render('index', {
-        title: 'Notes',
-        notes: titles
+        title: 'posts',
+        posts: titles
       });
     }
   });
