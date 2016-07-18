f265c76d1035ead79f1944bc4f354def977ca4a8
Zippi-wzp <truebatmandc@gmail.com>
2016-07-19 00:57:28 +0300

    Database model binds to routers

---
+++ modules/app/app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var post = require('./routes/post');
+var login = require('./routes/login');
+var debug = require('debug')('app4');
+
+var app = express();
+
+var model = require('./models-postgre/posts');
+model.connect("postgres://bruce_wayne:43873a65@localhost:5432/gothamdb", function (err) {
+  if (err) throw err;
+});
+
+[index, post, login].forEach(function (router) {
+  router.configure({model: model});
+});
+
+// view engine setup
+
+app.set('port', process.env.PORT || 7000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/', post);
+app.use('/', login);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+
+// module.exports = app;
