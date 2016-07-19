0e5f90894ab3ff16bf2a02e0af663bfe81fe3824
Zippi-wzp <truebatmandc@gmail.com>
2016-07-19 19:07:43 +0300

    Added handler for the transition to url '/login' from '/post/:id'

--- modules/app/public/javascripts/main.js
+++ modules/app/public/javascripts/main.js
@@ -1,16 +1,32 @@
 window.onload = function () {
-  var table = document.getElementsByTagName('table')[0];
-  table.onmouseover = function (event) {
-    event.target.style.cursor = "pointer";
-  }
-  table.addEventListener('click', function (event) {
-    event.preventDefault();
-    var tr = event.target.parentNode;
-    var id = tr.id;
+
+  var login = document.getElementsByClassName('btn-login')[0];
+
+  if (window.location.pathname == '/') {
+    var tbody = document.getElementsByTagName('tbody')[0];
+    var table = document.getElementsByTagName('table')[0];
+    tbody.addEventListener('mouseover', function (event) {
+      event.target.style.cursor = "pointer";
+    });
+
+    table.addEventListener('mousedown', function (event) {
+      event.preventDefault();
+    });
+
+    tbody.addEventListener('mouseup', function (event) {
+      event.preventDefault();
+      var tr = event.target.parentNode;
+      var id = tr.id;
       window.location.href = "/post/" + id;
-  });
-  var login = document.getElementsByClassName('login')[0];
-  login.addEventListener('click', function (event) {
-    window.location.href = "/login";
-  });
+    });
+
+    login.addEventListener('click', function (event) {
+      window.location.href += "login";
+    });
+
+  } else {
+    login.addEventListener('click', function (event) {
+      window.location.href = window.location.href + "/login";
+    });
+  }
 }
