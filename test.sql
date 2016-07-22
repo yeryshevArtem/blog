4f60b289619a8bb590846007e354607090be5fd4
Zippi-wzp <truebatmandc@gmail.com>
2016-07-22 19:55:07 +0300

    Added 'test.sql' file for creating default values to table

---
+++ test.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO posts (title, body) VALUES ('Strange bug ever in V8', 'Some info...1');
+
+INSERT INTO posts (title, body) VALUES ('Tracing de-optimizations in NodeJS', 'Some info...2');
+
+INSERT INTO posts (title, body) VALUES ('How to start contributing in NodeJS?', 'Some info...3');
+
+INSERT INTO posts (title, body) VALUES ('Profiling NodeJS applications', 'Some info...4');
+
+INSERT INTO posts (title, body) VALUES ('How V8 optimises JavaScript code?', 'Some info...5');
