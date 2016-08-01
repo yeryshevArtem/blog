1b456aa0987b9378e51966780cded6681d56315c
Zippi-wzp <truebatmandc@gmail.com>
2016-08-01 20:12:35 +0300

    Added 'loadData' module that provides an interface for loading data from server. Uses 'axios'.

---
+++ modules/admin/utils/loadData.js
@@ -0,0 +1,18 @@
+var axios = require('axios');
+var config = {
+  url: 'http://192.168.33.11:8000/api/posts',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+};
+
+var loadData = {
+  getPosts: function () {
+    return axios.get(config.url);
+  },
+  createPost: function (dataForPostRequest) {
+    return axios.post(config.url, dataForPostRequest, config);
+  }
+}
+
+module.exports = loadData;
