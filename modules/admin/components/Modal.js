541ee857e399501c0d8ed05bc9c8fbb95a7f8f78
Zippi-wzp <truebatmandc@gmail.com>
2016-08-04 16:04:11 +0300

    Added another template of modal window depending on input state.

--- modules/admin/components/Modal.js
+++ modules/admin/components/Modal.js
@@ -3,7 +3,7 @@ var Form = require('./Form');
 
 function Modal (props) {
   return (
-    <div className="modal fade" tabIndex="-1" role="dialog" id="myModal">
+    <div className="modal fade" tabIndex="-1" role="dialog" id="modalPrimary">
       <div className="modal-dialog" role="document">
         <div className="modal-content">
           <div className="modal-header">
@@ -11,12 +11,25 @@ function Modal (props) {
             <h4 className="modal-title">Modal title</h4>
           </div>
           <div className="modal-body">
-            <Form
-              formSubmited={props.saveChangeClicked}
-              changedValueTitle={props.changedTitle}
-              changedValueBody={props.changedBody}
-              valuesToForm={props.valuesToModal}
-              cancelClicked={props.cancel} />
+            {props.flagToDeleteForModalComp ?
+              <div>
+                <div>
+                  <p>If you click on the delete, all data will be deleted. Are you sure ?</p>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-primary" onClick={props.saveChangeClicked}>Save changes</button>
+                  <button type="button" className="btn btn-default" data-dismiss="modal" onClick={props.cancel}>Close</button>
+                </div>
+              </div> :
+              <div>
+                <Form
+                  formSubmited={props.saveChangeClicked}
+                  changedValueTitle={props.changedTitle}
+                  changedValueBody={props.changedBody}
+                  valuesToForm={props.valuesToModal}
+                  cancelClicked={props.cancel} />
+              </div>
+            }
           </div>
         </div>
       </div>
