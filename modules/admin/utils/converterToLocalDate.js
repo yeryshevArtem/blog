1792290f486161fd3362674b3edb658bfa9c25fb
Zippi-wzp <truebatmandc@gmail.com>
2016-08-05 21:56:51 +0300

    Created module to convert  UTC date format to local date format.

---
+++ modules/admin/utils/converterToLocalDate.js
@@ -0,0 +1,16 @@
+var converterToLocalDate = {
+  install: function (arrayOfPosts) {
+    arrayOfPosts.forEach(function (post, indexOfPost) {
+      post.createdAt = (new Date(post.createdAt)).toLocaleString();
+      post.modifiedAt = (new Date(post.modifiedAt)).toLocaleString();
+    });
+  },
+  create: function (post) {
+    post['created_at'] = (new Date(post['created_at'])).toLocaleString();
+    post['modified_at'] = (new Date(post['modified_at'])).toLocaleString();
+  },
+  update: function (post) {
+    post['modified_at'] = (new Date(post['modified_at'])).toLocaleString();
+  }
+}
+module.exports = converterToLocalDate;
