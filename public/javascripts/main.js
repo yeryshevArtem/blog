7e14291f7708ab7c9ea118764b81d27c8c95b75a
Zippi-wzp <truebatmandc@gmail.com>
2016-08-11 19:15:07 +0300

    Added sending the POST request to '/login' and added several new styles.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   var login = document.getElementsByClassName('btn-login')[0];
   var article = document.getElementsByClassName('post');
-  console.log(article);
   for (var i=0; i < article.length; i++) {
     article[i].addEventListener('mouseover', function (event) {
        event.target.style.cursor = "pointer";
@@ -10,12 +9,48 @@ window.onload = function () {
       event.preventDefault();
     });
     article[i].addEventListener('mouseup', function (event) {
+      var id = undefined;
       event.preventDefault();
-      var id = event.target.parentNode.id;
-      window.location.href = "/post/" + id;
+      if (event.target.nodeName === "ARTICLE") {
+        id = event.target.id;
+        window.location.href = "/post/" + id;
+      } else {
+        id = event.target.parentNode.id;
+        window.location.href = "/post/" + id;
+      }
     });
   }
   login.addEventListener('click', function (event) {
     window.location.href = "/login";
   });
+
+  if (window.location.pathname === '/login') {
+    var forms = document.getElementsByTagName('form')[0];
+    forms.addEventListener('submit', function (event) {
+      event.preventDefault();
+      var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+      var dataForSendToServer = {
+        username: username,
+        password: password
+      };
+      $.ajax({
+        url: '/login',
+        method: 'POST',
+        data: JSON.stringify(dataForSendToServer),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        statusCode: {
+          200: function (data) {
+            alert("Welcome, " + data.username);
+            window.location.href = "/";
+          },
+          403: function (jqXHR) {
+            var error = JSON.parse(jqXHR.responseText);
+            alert(error);
+          }
+        }
+      });
+    });
+  }
 }
