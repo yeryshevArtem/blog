6cfbf11afa88ba6feffe9f345db5c209a2050fb8
Zippi-wzp <truebatmandc@gmail.com>
2016-08-17 15:12:34 +0300

    Renamed the middleware from 'checkAuth' to 'checkForAdmin'.

--- modules/app/middleware/checkForAdmin.js
+++ modules/app/middleware/checkForAdmin.js
@@ -4,5 +4,5 @@ module.exports = function(req, res, next) {
   if (req.session.admin === "bruce_wayne") {
     return next();
   }
-  return next(new HttpError(401, "You are not admin!"));
+  return next(new HttpError(403, "You are not admin!"));
 };
